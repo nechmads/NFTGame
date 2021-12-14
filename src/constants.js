9ed2f02e4820c4aa46915bd87db02619aa13fe93
nechmads <me@shaharnechmad.com>
2021-12-14 21:01:12 +0200

    final project

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x918ef89d0E65D36fccfa6a6B09eC08CB870f2643";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
